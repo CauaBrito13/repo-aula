3b0b53bb8cd0b374d427e71dff15245a03b47de6
CauaBrito13 <cauaribeirodebrito7@gmail.com>
2026-03-20 20:28:11 -0300

    Modificações Funções

--- functions.php
+++ functions.php
@@ -1,3 +1,14 @@
 <?php
     echo "Vai Corinthians!";
+
+    function soma($num1, $num2)
+    {
+        return($num1 + $num2);
+    }
+
+
+    function raiz($num1)
+    {
+        return(sqrt($num1));
+    }
 ?>
