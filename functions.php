5a7169bfab24059daf1816985a488bc5baf36dc0
CauaBrito13 <cauaribeirodebrito7@gmail.com>
2026-03-20 21:10:08 -0300

    Revert "Modificações Funções"
    This reverts commit 3b0b53bb8cd0b374d427e71dff15245a03b47de6.

--- functions.php
+++ functions.php
@@ -1,14 +1,3 @@
 <?php
     echo "Vai Corinthians!";
-
-    function soma($num1, $num2)
-    {
-        return($num1 + $num2);
-    }
-
-
-    function raiz($num1)
-    {
-        return(sqrt($num1));
-    }
 ?>
